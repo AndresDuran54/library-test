b10825234a010df0d76baa192296354a48cc8909
Andrés Duran <andres_angeles@hotmail.es>
2024-09-06 17:56:33 -0500

    add package

---
+++ src/com/example/utils/Human.groovy
@@ -0,0 +1,7 @@
+package com.example.utils
+
+class Human {
+    static String reverse(String str) {
+        return str.reverse()
+    }
+}
--- vars/python.groovy
+++ vars/python.groovy
@@ -1,3 +1,14 @@
-def build() {
-    println "Hola"
+def checkoutCode() {
+    checkout scm
+}
+
+def poetryInstall() {
+    sh '''
+    pip install poetry
+    ~/.local/bin/poetry install
+    '''
+}
+
+def pytestRun() {
+    sh '~/.local/bin/poetry run python -m pytest'
 }
